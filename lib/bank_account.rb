b27d4902dbd94670da86d40c47d71fcaaf8470be
Olivia Auzenne <oauzenne@gmail.com>
2019-02-26 15:55:42 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,47 @@
 class BankAccount
+attr_reader :name
+attr_accessor :balance, :status
 
+@@all = []
+
+  def initialize (name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+    
+  def deposit(money)
+    self.balance += money
+  end
+  
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+  
+  def valid?
+    if status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+  
+  def close_account
+    status == "closed"
+  end
+  
+  
+  
+  
+  
+  
 end
+  
+  
+
+
